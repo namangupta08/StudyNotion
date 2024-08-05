7a1efcc373dff0fae146d9855937e3dd6070eaf8
namangupta08 <guptanaman032@gmail.com>
2024-08-05 20:57:26 +0530

    Profile Schema uodated

---
+++ server/models/Profile.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const profileSchema = mongoose.Schema({
+   gender:{
+    type:String,
+   },
+   dateOfBirth:{
+    type:String,
+   },
+   about:{
+    type:String,
+    trim:true,
+   },
+   contact:{
+    type:Number,
+    trim:true,
+   }
+});
+
+module.exports = mongoose.model("Profile" , profileSchema);
