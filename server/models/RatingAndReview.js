2b091dc0e17873cda59c35f393810d903b751371
namangupta08 <guptanaman032@gmail.com>
2024-09-22 00:58:22 +0530

    models updated

--- server/models/RatingAndReview.js
+++ server/models/RatingAndReview.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ratingAndReview = mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-    },
-    rating:{
-        type:Number,
-        required:true,
-    },
-    review:{
-        type:String,
-        required:true,
-    },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model("RatingAndReview" , ratingAndReview);
+module.exports = mongoose.model("RatingAndReview", ratingAndReview);
