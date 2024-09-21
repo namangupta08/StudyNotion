b285beb78278f5304072be2cf5b946bf5bc45ea9
namangupta08 <guptanaman032@gmail.com>
2024-09-21 22:53:37 +0530

    DB updated

--- server/config/database.js
+++ server/config/database.js
@@ -6,9 +6,9 @@ exports.connect = () => {
         useNewUrlParser:true,
         useUnifiedTopologu:true,
     })
-    .then(() => console.log("DB connected Successfully"))
+    .then(() => console.log("DB connected Successfully✅"))
     .catch((error) => {
-        console.log("Db connection failed")
+        console.log("Db connection failed❌")
         console.log(error);
         process.exit(1);
     })
