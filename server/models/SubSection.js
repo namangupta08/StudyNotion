80b076e272b6a077ffbde40c42b7600037a71f3f
namangupta08 <guptanaman032@gmail.com>
2024-09-21 23:02:44 +0530

    subsection model updated

--- server/models/SubSection.js
+++ server/models/SubSection.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const subSection = mongoose.Schema({
-   title:{
-    type:String,
-   },
-   timeDuration:{
-    type:String,
-   },
-   description:{
-    type:String,
-   },
-   vedioUrl:{
-    type:String,
-   }
+  title: {
+    type: String,
+  },
+  timeDuration: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  vedioUrl: {
+    type: String,
+  },
 });
 
-module.exports = mongoose.model("SubSection" , subSection);
+module.exports = mongoose.model("SubSection", subSection);
