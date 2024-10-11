dee660305dac8c1357518c4ca7f2089d3c02cc11
namangupta08 <guptanaman032@gmail.com>
2024-10-11 23:05:41 +0530

    paynet functionality

---
+++ server/controllers/Payments.js
@@ -0,0 +1,7 @@
+const {instance} = require('../config/razorpay');
+const Course = require('../models/Course');
+const User = require('../models/User');
+const mailSender = require('../utils/mailSender');
+const courseEnrollmentEmail = require('../mail/templates/courseEnrollmentEmail')
+
+
