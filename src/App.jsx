7a13b21f2c373f7d0cedd972c7960e663401ae09
namangupta08 <guptanaman032@gmail.com>
2024-10-20 00:11:18 +0530

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+     <Routes>
+      <Route path="/" element={</>}></Route>
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
