ce96ee74eaeb0a769b59320259b65b147c323436
namangupta08 <guptanaman032@gmail.com>
2024-10-20 18:27:20 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
      <Routes>
-      <Route path="/" element={</>}></Route>
+      <Route path="/" element={<Home/>}></Route>
      </Routes>
     </div>
   );
