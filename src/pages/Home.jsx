a3b99d6925782d537237ba30c3421ada911d3c3b
namangupta08 <guptanaman032@gmail.com>
2024-10-28 19:46:30 +0530

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,74 +59,104 @@ function Home() {
         <div>
           <CodeBlocks
             position={"lg:flex-row"}
-            heading=
-            {
+            heading={
               <div className="text-4xl font-semibold">
                 Unlock Your <HighlightText text={"coding potential"} /> with our
                 online courses
               </div>
             }
-            subheading=
-            {
+            subheading={
               "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
             }
-            ctabtn1=
-            {{
+            ctabtn1={{
               btnText: "Try it yourself",
               linkto: "/signup",
               active: true,
             }}
-            ctabtn2=
-            {{
+            ctabtn2={{
               btnText: "Learn More",
               linkto: "/login",
               active: false,
             }}
-            codeblock=
-            {`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
             codeColor={"text-yellow-25"}
-            backgroudGradient={"grad"}>
-          </CodeBlocks>
+            backgroudGradient={"grad"}
+          ></CodeBlocks>
         </div>
         <div>
-                <CodeBlocks
-                    position={"lg:flex-row-reverse"} 
-                    heading={
-                        <div className='text-4xl font-semibold'>
-                            Start <HighlightText text={"coding in seconds"}/> 
-                        </div>
-                    }
-                    subheading={"Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."}
-
-                    ctabtn1={
-                        {
-                            btnText:"Continue Lesson",
-                            linkto:"/login",
-                            active:true
-                        }
-                    }
-                    ctabtn2={
-                        {
-                            btnText:"Learn More",
-                            linkto:"/login",
-                            active:false
-                        }
-                    }
-                    codeblock={`<<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
-                    codeColor={"text-blue-100"}
-                    backgroudGradient={"grad2"}
-                >
-
-                </CodeBlocks>
-             </div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Start <HighlightText text={"coding in seconds"} />
+              </div>
+            }
+            subheading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              btnText: "Continue Lesson",
+              linkto: "/login",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+            codeColor={"text-blue-100"}
+            backgroudGradient={"grad2"}
+          ></CodeBlocks>
+        </div>
       </div>
 
       {/* Section2 */}
 
+      <div className="bg-pure-greys-5 text-richblack-700">
+        <div className="homepage_bg h-[360px]">
+          <div className="w-11/12 max-w-maxContent flex flex-col items-center justify-center gap-5 mx-auto">
+            <div className="h-[240px]"></div>
+            <div className="flex flex-row gap-7 text-white ">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-2">
+                  Explore full Catalog
+                  <FaArrowRight />
+                </div>
+              </CTAButton>
+
+              <CTAButton active={false} linkto={"/signup"}>
+                Book a Demo
+              </CTAButton>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7 mx-auto'>
+          <div className='flex flex-row gap-5 mb-10 mt-[95px] lg:mt-20 lg:flex-row lg:gap-0 justify-between '>
+          <div className='text-4xl font-semibold lg:w-[45%]'>
+                            Get the Skills you need for a
+                            <HighlightText text={"Job that is in demand"} />
+                        </div>
+
+                        <div className='flex flex-col w-[40%] items-start'>
+                            <div className='text-[16px] mb-10 font-semibold'>
+                                The modern StudyNotion is the dictates its own terms. Today, to
+                                be a competitive specialist requires more than professional
+                                skills.
+                            </div>
+                            <CTAButton active={true} linkto={"/signup"}>
+                                Learn More
+                            </CTAButton>
+                        </div>
+          </div>
+        </div>
+      </div>
+
       {/* Section3 */}
 
       {/* Footer */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
