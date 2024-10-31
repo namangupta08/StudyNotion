a9451bbd9080f6ae1b3ff801f78e7f7cbe74ca8a
namangupta08 <guptanaman032@gmail.com>
2024-10-31 20:22:27 +0530

    updation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -149,6 +149,7 @@ function Home() {
                                 Learn More
                             </CTAButton>
                         </div>
+
                         
 
           </div>
