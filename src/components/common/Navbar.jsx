a4e72e542ae5d98ffe9d7717dd592e143bb341e3
namangupta08 <guptanaman032@gmail.com>
2024-11-06 23:46:36 +0530

    navbar updated

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import { Link, matchPath, useLocation } from "react-router-dom";
+import logo from "../../assets/Logo/Logo-Full-Light.png";
+import { NavbarLinks } from "../../data/navbar-links";
+import { useSelector } from "react-redux";
 
 function Navbar() {
+  // const { token } = useSelector((state) => state.auth)
+  // const { user } = useSelector((state) => state.profile)
+  // const { totalItems } = useSelector((state) => state.cart)
+
+  const location = useLocation();
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
   return (
-    <div>
-      
+    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+      <div className="flex w-11/12 max-w-maxContent items-center justify-center">
+        {/* image */}
+        <Link to="/">
+          <img src={logo} alt="logo" width={160} height={42} loading="lazy" />
+        </Link>
+
+        {/* navbar */}
+        <nav>
+          <ul className="flex gap-x-3 text-richblack-25">
+            {NavbarLinks.map((link, index) => {
+              return (
+                <li key={index}>
+                  {link.title === "Catalog" ? (
+                    <div></div>
+                  ) : (
+                    <Link to={link?.path}>
+                      <p
+                        className={`${
+                          matchRoute(link?.path)
+                            ? "text-yellow-25"
+                            : "text-richblack-25"
+                        }`}
+                      >
+                        {link.title}
+                      </p>
+                    </Link>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        {/* login,signip,dashboard */}
+        <div className="flex gap-x-4 items-center">
+            
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
