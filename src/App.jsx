8baf0b6bdea9153477893f13935c1cce758309cd
namangupta08 <guptanaman032@gmail.com>
2024-11-18 22:01:01 +0530

    about us page updaed

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,25 @@ import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="flex flex-col bg-richblack-900 w-screen min-h-screen font-inter">
-    <Navbar/>
-     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-     </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="update-password/:id" element={<UpdatePassword />} />
+        <Route path="verify-email" element={<VerifyEmail />} />
+
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import signupImg from "../assets/Images/signup.webp"
 import Template from "../components/core/Auth/Template"
 
 function Signup() {
+    
   return (
     <Template
       title="Join the millions learning to code with StudyNotion for free"
